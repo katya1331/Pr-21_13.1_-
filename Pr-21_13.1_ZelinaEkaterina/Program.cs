5749d05612135052fe80278b943ce8d0a9352c97
katya1331 <152582001+katya1331@users.noreply.github.com>
2024-03-04 20:00:04 +0500

    Add files via upload

---
+++ Pr-21_13.1_ZelinaEkaterina/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Pr_21_13._1_ZelinaEkaterina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ArrayList list = new ArrayList();
+                list.AddRange(new string[] { "3.2", "21", "Студент", "Олег" });
+                ArrayList list1 = new ArrayList();
+                list1.AddRange(new string[] { "4.9", "27", "Рабочая", "Медведева" });
+                WriteLine("Введите с какой позиции хотите вставить новую коллекцию");
+                int n = int.Parse(ReadLine());
+                WriteLine("Введите какое количество элементов вы хотите удалить");
+                int k = int.Parse(ReadLine());
+                WriteLine("-------------------------------");
+                if (n > 0 && k > 1)
+                {
+                    list.InsertRange(n, list1);
+                    foreach (object e in list)
+                    {
+                        WriteLine(e);
+                    }
+                    WriteLine("-------------------------------");
+                    n = n / 2;
+                    WriteLine("Удаляем заданное кол-во символов");
+                    WriteLine("-------------------------------");
+                    for (int i = 0; i < k; i++)
+                    {
+                        list.RemoveAt(n);
+                    }
+                    foreach (object e in list)
+                    {
+                        WriteLine(e);
+                    }
+                    WriteLine("-------------------------------");
+                }
+                else
+                {
+                    WriteLine("error, недопустимые значения");
+                }
+
+                ReadKey();
+            }
+            catch
+            {
+                WriteLine("введен символ");
+            }
+        }
+    }
+}
